52b64e8107081689f211e3ac90f7fca13dbb694e
Jordan Schelcher <jschelcher2022@gmail.com>
2023-05-20 11:29:52 -0400

    uploaded code

---
+++ app.js
@@ -0,0 +1,58 @@
+let now = new Date();
+let h2 = document.querySelector("h2");
+
+let days =
+  ("Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday");
+
+let day = days[now.getDay()];
+let date = now.getDate();
+let year = now.getFullYear();
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "september",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+
+h2.innerHTML = `${day} <br> ${month} ${date}, ${year}`;
+
+function search(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-text-input");
+
+  let h3 = document.querySelector("h3");
+  h3.innerHTML = ` ${searchInput.value} `;
+}
+
+let form = document.querySelector("#search-form");
+
+form.addEventListener("submit", search);
+
+function currentWeather(response) {
+  let temp = response.data.main.temp;
+}
+
+let current = document.querySelector("current");
+
+current.innerHTML = `Currently ${temp}˚F`;
+
+let key = "d1a86552de255334f6117b348c4519bd";
+let url =
+  "`https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${key}&units=imperial`;";
+
+axios.get(url).then(currentWeather);
