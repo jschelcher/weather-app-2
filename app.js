b5ac869bcfab78bbb2d0fe017df41bdd4a626d8f
Jordan Schelcher <jschelcher2022@gmail.com>
2023-05-25 20:12:10 -0400

    Corrected the search function

--- app.js
+++ app.js
@@ -2,57 +2,55 @@ let now = new Date();
 let h2 = document.querySelector("h2");
 
 let days =
-  ("Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday");
+("Sunday",
+"Monday",
+"Tuesday",
+"Wednesday",
+"Thursday",
+"Friday",
+"Saturday");
 
 let day = days[now.getDay()];
 let date = now.getDate();
 let year = now.getFullYear();
 let months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "september",
-  "October",
-  "November",
-  "December",
+"January",
+"February",
+"March",
+"April",
+"May",
+"June",
+"July",
+"August",
+"september",
+"October",
+"November",
+"December"
 ];
 let month = months[now.getMonth()];
 
 h2.innerHTML = `${day} <br> ${month} ${date}, ${year}`;
 
 function search(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#search-text-input");
+event.preventDefault();
+let searchInput = document.querySelector("#search-text-input");
 
-  let h3 = document.querySelector("h3");
-  h3.innerHTML = ` ${searchInput.value} `;
+searchCity(searchInput.value);
 }
 
 let form = document.querySelector("#search-form");
 
 form.addEventListener("submit", search);
 
-function currentWeather(response) {
-  let temp = response.data.main.temp;
-}
-
-let current = document.querySelector("current");
-
-current.innerHTML = `Currently ${temp}˚F`;
-
+function searchCity(city){
 let key = "d1a86552de255334f6117b348c4519bd";
-let url =
-  "`https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${key}&units=imperial`;";
+let url =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=imperial`;
 
 axios.get(url).then(currentWeather);
+}
+
+function currentWeather(response) {
+let temp = response.data.main.temp;
+let h1 = document.querySelector("h1")
+h1.innerHTML = `Currently ${temp}˚F`;
+}
